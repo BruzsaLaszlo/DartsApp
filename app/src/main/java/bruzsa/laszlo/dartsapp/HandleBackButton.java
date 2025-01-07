ea7edcfb237592ab8c6a9a975b0fd878b110f337
Bruzsa László <lacika007@gmail.com>
2025-01-07 14:21:40 +0100

    handle back button

---
+++ app/src/main/java/bruzsa/laszlo/dartsapp/HandleBackButton.java
@@ -0,0 +1,31 @@
+package bruzsa.laszlo.dartsapp;
+
+import android.app.Activity;
+
+import androidx.activity.OnBackPressedCallback;
+
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+
+public class HandleBackButton extends OnBackPressedCallback {
+
+    private final Activity activity;
+
+    public HandleBackButton(Activity activity) {
+        super(true);
+        this.activity = activity;
+    }
+
+    @Override
+    public void handleOnBackPressed() {
+        new MaterialAlertDialogBuilder(activity)
+                .setTitle("Exit Game")
+                .setMessage("Are you sure?")
+                .setPositiveButton("EXIT", (dialog, which) -> {
+                    remove();
+                    activity.onBackPressed();
+                })
+                .setNegativeButton("CANCEL", (dialog, which) -> handleOnBackCancelled())
+                .show();
+    }
+
+}
