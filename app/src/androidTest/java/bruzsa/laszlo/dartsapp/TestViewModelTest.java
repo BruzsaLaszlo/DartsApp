458eb2444ba686d261df51f545f60fb65c8dbd54
Bruzsa László <lacika007@gmail.com>
2025-01-08 23:20:53 +0100

    hilt example test

--- app/src/androidTest/java/bruzsa/laszlo/dartsapp/TestViewModelTest.java
+++ app/src/androidTest/java/bruzsa/laszlo/dartsapp/TestViewModelTest.java
@@ -2,6 +2,7 @@ package bruzsa.laszlo.dartsapp;
 
 import static org.junit.Assert.assertEquals;
 
+import androidx.lifecycle.ViewModel;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 
@@ -9,11 +10,30 @@ import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
+import javax.inject.Inject;
+
+import bruzsa.laszlo.dartsapp.model.home.AppSettings;
+import dagger.hilt.android.lifecycle.HiltViewModel;
 import dagger.hilt.android.testing.HiltAndroidRule;
 import dagger.hilt.android.testing.HiltAndroidTest;
 
 @HiltAndroidTest
 public class TestViewModelTest {
+
+    @HiltViewModel
+    public static class TestViewModel extends ViewModel {
+
+        private final AppSettings settings;
+
+        @Inject
+        public TestViewModel(AppSettings settings) {
+            this.settings = settings;
+        }
+
+        public AppSettings getSettings() {
+            return settings;
+        }
+    }
 
     @Rule(order = 1)
     public HiltAndroidRule hiltRule = new HiltAndroidRule(this);
